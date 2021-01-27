f2823c55df893984d6eaf3c2316a8b6678c8ca3d
Kirill <kirillik95@yandex.ru>
2021-01-27 18:24:24 +0300

    test: write simple test :)

--- src/city/city.service.spec.ts
+++ src/city/city.service.spec.ts
@@ -1,18 +1,62 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CityService } from './city.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CityModel } from './city.model';
+import { CityService } from './city.service';
+import * as faker from 'faker';
 
-// describe('CityService', () => {
-//   let service: CityService;
+class CityRepositoryFake {
+  public create(): void {}
+}
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [CityService],
-//     }).compile();
+describe('CityService', () => {
+  let service: CityService;
+  let repository: Repository<CityModel>;
 
-//     service = module.get<CityService>(CityService);
-//   });
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CityService,
+        {
+          provide: getRepositoryToken(CityModel),
+          useClass: CityRepositoryFake,
+        },
+      ],
+    }).compile();
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+    service = module.get<CityService>(CityService);
+    repository = module.get(getRepositoryToken(CityModel));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a city', async () => {
+      const randomId = faker.random.uuid();
+      const createdCity: CityModel = {
+        id: randomId,
+        name: 'NN',
+        phone: '88005553535',
+        stations: [],
+      };
+
+      jest.spyOn(service, 'create').mockResolvedValue(createdCity);
+      expect(
+        await service.create({
+          name: 'NN',
+          phone: '88005553535',
+        }),
+      ).toBe(createdCity);
+    });
+  });
+
+  //   describe('get', () => {});
+
+  //   describe('update', () => {});
+
+  //   describe('delete', () => {});
+
+  //   it('should return all cities', () => {});
+});
