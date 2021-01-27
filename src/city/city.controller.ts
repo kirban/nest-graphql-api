571af142f787db3451efc74908eb76088b997b1d
Kirill <kirillik95@yandex.ru>
2021-01-27 15:53:08 +0300

    refactor: remove redunant files

--- src/city/city.controller.ts
+++
@@ -1,7 +0,0 @@
-// import { Controller, Query, Get } from '@nestjs/common';
-
-// @Controller('city')
-// export class CityController {
-//   @Get()
-//   findAll(@Query() query: ListAllEntities) {}
-// }
--- src/station/station.controller.ts
+++
@@ -1,6 +0,0 @@
-// import { Controller } from '@nestjs/common';
-
-// @Controller('station')
-// export class StationController {
-    
-// }
